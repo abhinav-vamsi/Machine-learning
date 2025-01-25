ebb1f22b9bedae8008b5c6eda2b4338e50edf708
Abhinav Vamsi <abhinavvamsi@Abhinavs-MacBook-Air.local>
2025-01-25 18:24:09 +0530

    Your commit message

--- test.py
+++ test.py
@@ -2,3 +2,7 @@ import pandas as pd
 import numpy as np
 dataset=pd.read_csv(r'/Users/abhinav/Downloads/bank_transactions_data_2.csv')
 print(dataset.head)
+x=dataset.iloc[:,:-1].values
+y=dataset.iloc[:,-1].values
+print(x)
+print(y)
